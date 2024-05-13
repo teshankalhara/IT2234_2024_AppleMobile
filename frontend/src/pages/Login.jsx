d9b947c1363d109be504f3ade061e25e3900ffe1
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-14 01:03:16 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
             <div className='mx-auto container p-4'>
                 <div className='bg-white p-5 w-full max-w-sm mx-auto roun'>
                     <div className='w-20 h-20 mx-auto'>
-                        <img src={loginIcons} alt='login icons' />
+                        <img className='rounded-full' src={loginIcons} alt='login icons' />
                     </div>
 
                     <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
