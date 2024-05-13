d0e2a015e156959ed0a194ed54b44baef756f934
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-14 01:38:35 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import { Provider  } from 'react-redux'
+import { store } from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+  //<React.StrictMode>
+  <Provider store={store}>
+      <RouterProvider router={router}/>
+  </Provider>
+  //</React.StrictMode>,
 )
