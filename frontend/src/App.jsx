23c5f951f62962261302939dbbe7beb3d5f63401
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-14 01:38:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,27 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import SummaryApi from "./api";
 import Context from "./context";
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from "./store/userSlice";
 
 
 function App() {
+  const dispatch = useDispatch()
   const fetchUserDetails=async()=>{
     const dataResponse=await fetch(SummaryApi.current_user,{
       method:SummaryApi.current_user.method,
       credentials:"include"
     })
     const dataApi=await dataResponse.json()
+
+    if(dataApi.success){
+      dispatch(setUserDetails(dataApi.data))
+    }
+
     console.log("data-user:",dataResponse)
   }
   useEffect(()=>{
-
+    fetchUserDetails()
   },[])
 
   return (
