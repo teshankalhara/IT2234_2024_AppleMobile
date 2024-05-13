b9abe083a73ae3f9d358f671eb768f022be72952
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-13 21:22:22 +0530

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='bg-slate-200 '>
+            <div className='container mx-auto'>
+                <p className='text-center font-bold'>Vite+React&copy;2024</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
