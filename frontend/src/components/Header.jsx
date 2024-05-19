2c8f49133c751a73583dc87ec9591921d4c79627
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-20 03:11:39 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,45 +1,92 @@
-import React from 'react'
-import Logo from './Logo'
-import { GrSearch } from 'react-icons/gr'
-import { FaRegCircleUser,FaCartShopping } from 'react-icons/fa6'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser, FaCartShopping } from "react-icons/fa6";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import SummaryApi from "../api";
+import { toast } from "react-toastify";
+import { setUserDetails } from "../store/userSlice";
+import ROLE from "../api/role";
+import Context from "../context";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state?.user?.user);
+  const dispatch = useDispatch();
+  const handleLogout = async () => {
+    const fetchData = await fetch(SummaryApi.logout_user.url, {
+      method: SummaryApi.logout_user.method,
+      credentials: "include",
+    });
+
+    const data = await fetchData.json();
+
+    if (data.success) {
+      toast.success(data.message);
+      dispatch(setUserDetails(null));
+      navigate("/");
+    }
+
+    if (data.error) {
+      toast.error(data.message);
+    }
+  };
   return (
-    <header className='h-16 shadow-md bg-white '>
-        <div className='h-full container mx-auto flex items-center px-4 justify-between'>
-            <div className=''>
-                <Link to={"/"}>
-                    <Logo w={50} h={50}/>
-                </Link>
-            </div>
-
-            <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2'>
-                <input type='text' placeholder='Search Product Here' className='w-full outline-none pl-2'/>
-                <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
-                    <GrSearch/>
-                </div>
-            </div>
-
-            <div className='flex items-center gap-7'>
-                <div className='text-3xl cursor-pointer'>
-                    <FaRegCircleUser/>
-                </div>
-                
-                <div className='text-2xl relative'>
-                    <span><FaCartShopping/></span>
-                    <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-2'>
-                        <p className='text-xs'>0</p>
-                    </div>
-                </div>
-
-                <div>
-                    <Link to={"/login"} className='px-3 py-1 pb-2 rounded-full text-white font-bold text-sm bg-red-600 hover:bg-red-700'>Login</Link>
-                </div>
-            </div>
+    <header className="h-16 shadow-md bg-white ">
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+        <div className="">
+          <Link to={"/"}>
+            <Logo w={50} h={50} />
+          </Link>
         </div>
-    </header>
-  )
-}
 
-export default Header
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
+          <input
+            type="text"
+            placeholder="Search Product Here"
+            className="w-full outline-none pl-2"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+            <GrSearch />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-7">
+          <div className="text-3xl cursor-pointer">
+            <FaRegCircleUser />
+          </div>
+
+          <div className="text-2xl relative">
+            <span>
+              <FaCartShopping />
+            </span>
+            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-2">
+              <p className="text-xs">0</p>
+            </div>
+          </div>
+
+          <div>
+            {user?._id ? (
+              <button
+                onClick={handleLogout}
+                className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700"
+              >
+                Logout
+              </button>
+            ) : (
+              <Link
+                to={"/login"}
+                className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
