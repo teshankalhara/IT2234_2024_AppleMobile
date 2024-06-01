85be2f37b509e41e303882535435e442b9954d96
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-02 02:20:18 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,17 @@ const express=require('express')
 
 const router=express.Router()
 
+const authToken = require('../middleware/authToken')
+
 const userSignUpController=require('../controller/userSignUp')
 const userSignInController = require('../controller/userSignIn')
-const authToken = require('../middleware/authToken')
 const userDetailsController = require('../controller/userDetails')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
 const updateUser = require('../controller/updateUser')
+
+const updateUser = require('../controller/addToCartController')
+
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -19,5 +23,7 @@ router.get("/userLogout",userLogout)
 router.get("/all-user", authToken, allUsers)
 router.post("/update-user", authToken, updateUser)
 //adminPanel
+
+router.post("/upload-product",authToken,addToCartController)
 
 module.exports=router
