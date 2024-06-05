72094c97c9a9c64cb880482bc5a2429d73c34dc6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-05 18:51:18 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import CategoryList from '../components/CategoryList'
+import BannerProduct from '../components/BannerProduct'
+import HorizontalCardProduct from '../components/HorizontalCardProduct'
+import VerticalCardProduct from '../components/VerticalCardProduct'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <CategoryList/>
+      <BannerProduct/>
+
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
+      <HorizontalCardProduct category={"mobiles"} heading={"Popular's Mobiles"}/>
+
+      <VerticalCardProduct category={"watches"} heading={"Mobiles"}/>
+      <VerticalCardProduct category={"macbook"} heading={"MacBooks"}/>
+      <VerticalCardProduct category={"ipad"} heading={"Ipads"}/>
+      <VerticalCardProduct category={"chargers"} heading={"Chargers"}/>
+      <VerticalCardProduct category={"cables"} heading={"Cables"}/>
+      <VerticalCardProduct category={"covers"} heading={"Covers"}/>
+    </div>
   )
 }
 
