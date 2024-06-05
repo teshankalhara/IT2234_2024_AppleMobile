c950b22632fa331e7ab77366d26dc455f6fa44dd
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-05 18:50:23 +0530

    Update AdminPanel.jsx

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -1,49 +1,56 @@
 import React, { useEffect } from "react";
-import { FaRegCircleUser, FaCartShopping } from "react-icons/fa6";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
-import ROLE from "../api/role";
+import { useSelector } from "react-redux";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import ROLE from "../common/role";
 
 const AdminPanel = () => {
-    const user = useSelector((state) => state?.user?.user);
-    const navigate = useNavigate()
-    useEffect(() => {
-        if(user?.role !== ROLE.ADMIN) {
-            navigate("/")
-        }
-        
-    },[user])
-    return (
-        <div className="min-h-[calc(100hv-120px)] md-flex hidden">
+  const user = useSelector((state) => state?.user?.user);
+  const navigate = useNavigate();
 
-            <aside className="bg-white min-full w-full max-w-60 customShadow">
-                <div className="h-32 flex justify-center items-center">
-                    <div className='text-3xl cursor-pointer relative flex justify-center'>
-                    {
-                        user?.profilePic ? (
-                        <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
-                        ) : (
-                        <FaRegCircleUser />
-                        )
-                    }
-                    </div>
-                    <p className="capitalize text-lg font-semibold">{user?.name}</p>
-                    <p className="text-sm">{user?.role}</p>
-                </div>
-                {/*navigation*/}
-                <div>
-                    <nav>
-                        <Link to = {'all-users'} className="px-2 py-1 hover:bg-slate-100">All Users</Link>
-                        <Link to = {'all-products'} className="px-2 py-1 hover:bg-slate-100">All Products</Link>
-                    </nav>
-                </div>
-            </aside>
+  useEffect(() => {
+    if (user?.role !== ROLE.ADMIN) {
+      navigate("/");
+    }
+  }, [user]);
 
-            <main className="w-full h-full p-2">
-                <Outlet/>
-            </main>
+  return (
+    <div className="min-h-[calc(100vh-120px)] md:flex hidden">
+      <aside className="bg-white min-h-full  w-full  max-w-60 customShadow">
+        <div className="h-32  flex justify-center items-center flex-col">
+          <div className="text-5xl cursor-pointer relative flex justify-center">
+            {user?.profilePic ? (
+              <img
+                src={user?.profilePic}
+                className="w-20 h-20 rounded-full"
+                alt={user?.name}
+              />
+            ) : (
+              <FaRegCircleUser />
+            )}
+          </div>
+          <p className="capitalize text-lg font-semibold">{user?.name}</p>
+          <p className="text-sm">{user?.role}</p>
         </div>
-    )
-}
+
+        {/***navigation */}
+        <div>
+          <nav className="grid p-4">
+            <Link to={"all-users"} className="px-2 py-1 hover:bg-slate-100">
+              All Users
+            </Link>
+            <Link to={"all-products"} className="px-2 py-1 hover:bg-slate-100">
+              All product
+            </Link>
+          </nav>
+        </div>
+      </aside>
+
+      <main className="w-full h-full p-2">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 export default AdminPanel;
