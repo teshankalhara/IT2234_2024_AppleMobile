71fe9a0bf2c7dd8e793d3752d245c9f6f108a6d6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-05 18:50:30 +0530

    Create productCategory.js

---
+++ frontend/src/helpers/productCategory.js
@@ -0,0 +1,17 @@
+const productCategory = [
+    { id : 1, label : "Airpodes", value : "airpodes"},
+    { id : 2, label : "Mobiles", value : "mobiles"},
+    { id : 3, label : "Watches", value : "watches"},
+    { id : 4, label : "MacBook", value : "macbook"},
+    { id : 5, label : "Ipad", value : "ipad"},
+    { id : 6, label : "Cables", value : "cables"},
+    { id : 7, label : "Chargers", value : "chargers"},
+    { id : 8, label : "Tempards", value : "tempards"},
+    { id : 9, label : "Covers", value : "covers"},
+    { id : 10, label : "Rings", value : "rings"},
+    { id : 11, label : "Powerbanks", value : "powerbanks"},
+    { id : 12, label : "Sticks", value : "sticks"},
+]
+
+
+export default productCategory
