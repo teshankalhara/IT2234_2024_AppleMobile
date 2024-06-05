19200dff2cd3338ee4a9434a887bdd76324175b5
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-05 18:49:15 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Logo from "./Logo";
 import { GrSearch } from "react-icons/gr";
-import { FaRegCircleUser, FaCartShopping } from "react-icons/fa6";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import SummaryApi from "../api";
+import SummaryApi from "../common";
 import { toast } from "react-toastify";
 import { setUserDetails } from "../store/userSlice";
-import ROLE from "../api/role";
+import ROLE from "../common/role";
 import Context from "../context";
 
 const Header = () => {
-  const navigate = useNavigate()
   const user = useSelector((state) => state?.user?.user);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [menuDisplay, setMenuDisplay] = useState(false);
-  //console.log("user heaeder",user)
+  const context = useContext(Context);
+  const navigate = useNavigate();
+  const searchInput = useLocation();
+  const URLSearch = new URLSearchParams(searchInput?.search);
+  const searchQuery = URLSearch.getAll("q");
+  const [search, setSearch] = useState(searchQuery);
+
   const handleLogout = async () => {
     const fetchData = await fetch(SummaryApi.logout_user.url, {
       method: SummaryApi.logout_user.method,
@@ -34,20 +40,33 @@ const Header = () => {
       toast.error(data.message);
     }
   };
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+
+    if (value) {
+      navigate(`/search?q=${value}`);
+    } else {
+      navigate("/search");
+    }
+  };
   return (
-    <header className="h-16 shadow-md bg-white ">
-      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+    <header className="h-16 shadow-md bg-white fixed w-full z-40">
+      <div className=" h-full container mx-auto flex items-center px-4 justify-between">
         <div className="">
           <Link to={"/"}>
             <Logo w={50} h={50} />
           </Link>
         </div>
 
-        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
           <input
             type="text"
-            placeholder="Search Product Here"
-            className="w-full outline-none pl-2"
+            placeholder="search product here..."
+            className="w-full outline-none"
+            onChange={handleSearch}
+            value={search}
           />
           <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
             <GrSearch />
@@ -55,43 +74,52 @@ const Header = () => {
         </div>
 
         <div className="flex items-center gap-7">
-          <div className="text-3xl cursor-pointer">
-            {
-              user?._id && (
-                <div className='text-3xl cursor-pointer relative flex justify-center' onClick={() => setMenuDisplay(preve => !preve)}>
-                  {
-                    user?.profilePic ? (
-                      <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
-                    ) : (
-                      <FaRegCircleUser />
-                    )
-                  }
-                </div>
-              )
-            }
-          </div>
-            {
-              menuDisplay && (
-                <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
-                  <nav>
-                    {
-                      user?.role === ROLE.ADMIN && (
-                        <Link to={'/admin-panel/all-products'} className="whitespace-nowrap hidden md:block hover:bg-slate-100 p-2" onClick={() => setMenuDisplay(preve => !preve)}>Admin Panel</Link>
-                      )
-                    }
-                  </nav>
-                </div>
-              )
-            }
+          <div className="relative flex justify-center">
+            {user?._id && (
+              <div
+                className="text-3xl cursor-pointer relative flex justify-center"
+                onClick={() => setMenuDisplay((preve) => !preve)}
+              >
+                {user?.profilePic ? (
+                  <img
+                    src={user?.profilePic}
+                    className="w-10 h-10 rounded-full"
+                    alt={user?.name}
+                  />
+                ) : (
+                  <FaRegCircleUser />
+                )}
+              </div>
+            )}
 
-          <div className="text-2xl relative">
-            <span>
-              <FaCartShopping />
-            </span>
-            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-2">
-              <p className="text-xs">0</p>
-            </div>
+            {menuDisplay && (
+              <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
+                <nav>
+                  {user?.role === ROLE.ADMIN && (
+                    <Link
+                      to={"/admin-panel/all-products"}
+                      className="whitespace-nowrap hidden md:block hover:bg-slate-100 p-2"
+                      onClick={() => setMenuDisplay((preve) => !preve)}
+                    >
+                      Admin Panel
+                    </Link>
+                  )}
+                </nav>
+              </div>
+            )}
           </div>
+
+          {user?._id && (
+            <Link to={"/cart"} className="text-2xl relative">
+              <span>
+                <FaShoppingCart />
+              </span>
+
+              <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+                <p className="text-sm">{context?.cartProductCount}</p>
+              </div>
+            </Link>
+          )}
 
           <div>
             {user?._id ? (
