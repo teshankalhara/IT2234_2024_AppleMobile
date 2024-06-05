1c1fe55e0d8b27165f932487dd6b47d13cdec3d7
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-05 18:51:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,69 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { createBrowserRouter, Outlet } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import SummaryApi from "./api";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SummaryApi from "./common";
 import Context from "./context";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { setUserDetails } from "./store/userSlice";
-
 
 function App() {
   const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
 
   const fetchUserDetails = async()=>{
-    const dataResponse = await fetch(SummaryApi.current_user.url,{
-      method : SummaryApi.current_user.method,
+      const dataResponse = await fetch(SummaryApi.current_user.url,{
+        method : SummaryApi.current_user.method,
+        credentials : 'include'
+      })
+
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        dispatch(setUserDetails(dataApi.data))
+      }
+  }
+
+  const fetchUserAddToCart = async()=>{
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
       credentials : 'include'
     })
 
     const dataApi = await dataResponse.json()
 
-    if(dataApi.success){
-      dispatch(setUserDetails(dataApi.data))
-    }
-}
-  
-  useEffect(()=>{
-    fetchUserDetails()
-  },[])
+    setCartProductCount(dataApi?.data?.count)
+  }
 
+  useEffect(()=>{
+    /**user Details */
+    fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
+
+  },[])
   return (
     <>
       <Context.Provider value={{
-          fetchUserDetails//user details fetch
+          fetchUserDetails, // user detail fetch 
+          cartProductCount, // current user add to cart product count,
+          fetchUserAddToCart
       }}>
-        <ToastContainer />
+        <ToastContainer 
+          position='top-center'
+        />
+        
         <Header/>
-        <Outlet/>
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet/>
+        </main>
         <Footer/>
       </Context.Provider>
     </>
   );
 }
 
-export default App
+export default App;
