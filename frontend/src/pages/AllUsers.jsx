99c620a8d5136c8023a17af070edbdd423d24076
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-06-08 00:54:13 +0530

    Update AllUsers.jsx

--- frontend/src/pages/AllUsers.jsx
+++ frontend/src/pages/AllUsers.jsx
@@ -2,27 +2,31 @@ import React, { useEffect, useState } from "react";
 import SummaryApi from "../common";
 import { toast } from "react-toastify";
 import moment from "moment";
+import { MdModeEdit } from "react-icons/md";
 import ChangeUserRole from "../components/ChangeUserRole";
 
 const AllUsers = () => {
   const [allUser, setAllUsers] = useState([]);
   const [openUpdateRole, setOpenUpdateRole] = useState(false);
-  const [updateUserDetails, setUpdateUserDetails] = useState(
-    (email = ""),
-    (name = ""),
-    (role = ""),
-    (_id = "")
-  );
+  const [updateUserDetails, setUpdateUserDetails] = useState({
+    email: "",
+    name: "",
+    role: "",
+    _id: "",
+  });
 
   const fetchAllUsers = async () => {
-    const fetchData = await fetch(SummaryApi.alluser.url, {
+    const fetchData = await fetch(SummaryApi.allUser.url, {
       method: SummaryApi.allUser.method,
       credentials: "include",
     });
+
     const dataResponse = await fetchData.json();
+
     if (dataResponse.success) {
       setAllUsers(dataResponse.data);
     }
+
     if (dataResponse.error) {
       toast.error(dataResponse.message);
     }
@@ -36,8 +40,8 @@ const AllUsers = () => {
     <div className="bg-white pb-4">
       <table className="w-full userTable">
         <thead>
-          <tr className="bg-black text-white">
-            <th>Sr.</th>
+          <tr className="text-white bg-gray-500">
+            <th>No</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
@@ -45,7 +49,7 @@ const AllUsers = () => {
             <th>Action</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="">
           {allUser.map((el, index) => {
             return (
               <tr>
@@ -53,7 +57,7 @@ const AllUsers = () => {
                 <td>{el?.name}</td>
                 <td>{el?.email}</td>
                 <td>{el?.role}</td>
-                <td>{moment(el?.createdAt).format("ll")}</td>
+                <td>{moment(el?.createdAt).format("LL")}</td>
                 <td>
                   <button
                     className="bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white"
@@ -70,6 +74,7 @@ const AllUsers = () => {
           })}
         </tbody>
       </table>
+
       {openUpdateRole && (
         <ChangeUserRole
           onClose={() => setOpenUpdateRole(false)}
